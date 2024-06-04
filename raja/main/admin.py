9551ef56e8d7336e98413f0e6fc31033ebd98f13
Rajasimhareddybolla <rajasimhabolla@gmail.com>
2024-06-04 17:18:34 +0530

    create commit 1

--- raja/main/admin.py
+++ raja/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MyUser, Medical_stat, Specialist, Hospital, Review, Appointment, Symptom
 
-# Register your models here.
+admin.site.register(MyUser)
+admin.site.register(Medical_stat)
+admin.site.register(Specialist)
+admin.site.register(Hospital)
+admin.site.register(Review)
+admin.site.register(Appointment)
+admin.site.register(Symptom)
--- raja/main/models.py
+++ raja/main/models.py
@@ -1,5 +1,71 @@
 from django.db import models
 
+
+
+
 class MyUser(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=15)
+    def __str__(self) -> str:
+        return self.username
+
+class Medical_stat(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='medical_stats')
+    age = models.PositiveIntegerField()
+    blood_pressure = models.CharField(max_length=100)
+    diabetes = models.BooleanField(default=False)
+    prev_stroke = models.DateField(null=True, blank=True)
+    cholesterol_level = models.PositiveIntegerField()
+    family_ethnicity = models.TextField()
+
+
+class Specialist(models.Model):
+    class Specialization(models.TextChoices):
+        CARDIOLOGIST = 'CR', 'Cardiologist'
+        DERMATOLOGIST = 'DR', 'Dermatologist'
+        NEUROLOGIST = 'NR', 'Neurologist'
+        
+        # Add more specializations as needed
+
+    name = models.CharField(max_length=200)
+    specialized = models.CharField(
+        max_length=2,
+        choices=Specialization.choices,
+        default=Specialization.CARDIOLOGIST,
+    )
+    description = models.TextField()
+    education = models.TextField()
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    location = models.TextField()
+    specialists = models.ManyToManyField(Specialist)
+    stats = models.TextField()
+    description = models.TextField()
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name="appointments")
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name="appointments")
+    datetime = models.DateTimeField()
+    comments = models.TextField()
+
+class Symptom(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name="symptoms")
+    symptoms = models.TextField()
+    date = models.DateField()
+    name = models.CharField(max_length=255)
+    url = models.TextField()
+    location = models.TextField()
+    specialists = models.ManyToManyField(Specialist)
+    stats = models.TextField()
+    description = models.TextField()
+
+class Review(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name="reviews")
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, related_name= "reviews")
+    STARS = [(i, i) for i in range(1, 6)]
+    stars = models.IntegerField(choices=STARS)
+    comments = models.TextField()
